004509b3b23fe8e25223c3663253c22217729d19
SkyDiver <lvv10@yandex.ru>
2026-01-23 22:26:12 +0300

    доработка функционала по ФЗ 16 fix post tests 1

--- server/src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -65,6 +70,7 @@ public class GlobalExceptionHandler {
         log.error("Ошибка валидации аргументов: {}", message);
         return Map.of("error", message);
     }
+
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
